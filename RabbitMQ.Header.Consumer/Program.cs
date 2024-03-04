f86de9fd311ac21ce31037cb46a4d9aba26d1a27
Emircan <emircandv787@gmail.com>
2024-03-04 05:35:45 +0300

    header exchange added

---
+++ RabbitMQ.Header.Consumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Header.Consumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new();
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                exchange: "header-exchange-example",
+                type: ExchangeType.Headers);
+
+
+            Console.WriteLine("lütfen header valuesini giriniz : ");
+            string value = Console.ReadLine();
+            string queueName = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(
+                queue: queueName,
+                exchange: "header-exchange-example",
+                routingKey: string.Empty,
+                new Dictionary<string, object>
+                {
+                    //["x-match"] ="any",
+                    //["x-match"] ="all",
+                    ["no"] = value
+                });
+
+            EventingBasicConsumer consumer = new(channel);
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+
+            consumer.Received += (sender, e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.Span);
+                Console.WriteLine(message);
+                
+            };
+
+        }
+    }
+}
---
+++ RabbitMQ.Header.Publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Header.Publisher
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new();
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                exchange: "header-exchange-example",
+                type: ExchangeType.Headers);
+
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Delay(100).Wait();
+
+                var message = Encoding.UTF8.GetBytes($"Merhaba {i}");
+                Console.WriteLine("Lütfen header valuesini girin");
+                var value = Console.ReadLine();
+                IBasicProperties basicProperties = channel.CreateBasicProperties();
+                basicProperties.Headers = new Dictionary<string, object>()
+                {
+                    ["no"] = value
+                };
+
+                channel.BasicPublish(
+                    exchange: "header-exchange-example",
+                    routingKey: string.Empty,
+                    body : message,
+                    basicProperties: basicProperties);
+
+            }
+
+        }
+    }
+}
